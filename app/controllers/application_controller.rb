97b431db7bb5b92c5b2a1dd2943941acf04464e7
Anthony Ladouce <anthony.ladouce@gmail.com>
2017-12-31 10:47:31 +0100

    MAJ Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # custom 404
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActiveRecord::RecordNotFound,
+                ActionController::RoutingError,
+                ActionController::UnknownController,
+                Exception,
+                ActionController::MethodNotAllowed do |excption|
+
+      # Put loggers here, if desired.
+
+      redirect_to root_path
+    end
+  end
 end
